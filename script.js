503871e92ce654df8bce0882832ba3c75d87b44c
Omkarrawool03 <138473470+Omkarrawool03@users.noreply.github.com>
2023-11-21 00:22:06 +0530

    finalised

---
+++ script.js
@@ -0,0 +1,7 @@
+// const slides = document.querySelectorAll(".slides")
+// var counter = 0;
+// slides.forEach(
+//     (slides, index) => {
+//         slides.style.left = "$(index * 100)%"
+//     }
+// )
